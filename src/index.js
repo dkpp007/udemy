fc9f38239727634ed6250d3bd7bd301cbfa753f7
dkpp007 <95537484+dkpp007@users.noreply.github.com>
2022-12-15 15:55:22 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const name = "Daniel";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+ReactDOM.render(
+  <div>
+    <p>Created by {name}!</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
